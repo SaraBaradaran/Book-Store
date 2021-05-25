c61dcb4a9edd802fcdffdfebb8fe5d684edb5821
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2021-05-25 20:36:35 +0430

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,3 +1,13 @@
+/****************************************************************  
+#								
+# 								
+# 	Creator Name:   Sara Baradaran, Mahdi Heidari		
+# 	Create Date:    Nov 2018 				
+# 	Module Name:    main.cpp
+# 	Project Name:   Bookstore	
+#								
+#								
+****************************************************************/
 
 #include <QApplication>
 #include "librarysystem.h"
