61858bf8730e01dadcbb9d93253983f6b254e463
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2021-05-25 20:16:06 +0430

    Update getfactor.h

--- headers/getfactor.h
+++ headers/getfactor.h
@@ -1,3 +1,14 @@
+/****************************************************************  
+#								
+# 								
+# 	Creator Name:   Sara Baradaran, Mahdi Heidari		
+# 	Create Date:    Jun 2018 				
+# 	Module Name:    getfactor.h
+# 	Project Name:   IUT_Messenger	
+#								
+#								
+****************************************************************/
+
 #ifndef GETFACTOR_H
 #define GETFACTOR_H
 
