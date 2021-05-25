9c4ac70ff73237ff70fd01a43451baff02bb5c2c
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2021-05-25 20:15:48 +0430

    Update customer.h

--- headers/customer.h
+++ headers/customer.h
@@ -1,3 +1,14 @@
+/****************************************************************  
+#								
+# 								
+# 	Creator Name:   Sara Baradaran, Mahdi Heidari		
+# 	Create Date:    Jun 2018 				
+# 	Module Name:    customer.h
+# 	Project Name:   IUT_Messenger	
+#								
+#								
+****************************************************************/
+
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 
