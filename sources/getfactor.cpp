13ae83bb2a6bdd4927707cef6e88cdccf676d3e0
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2021-05-25 20:34:59 +0430

    Update getfactor.cpp

--- sources/getfactor.cpp
+++ sources/getfactor.cpp
@@ -1,3 +1,14 @@
+/****************************************************************  
+#								
+# 								
+# 	Creator Name:   Sara Baradaran, Mahdi Heidari		
+# 	Create Date:    Nov 2018 				
+# 	Module Name:    getfactor.cpp
+# 	Project Name:   Bookstore	
+#								
+#								
+****************************************************************/
+
 #include "getfactor.h"
 #include "librarysystem.h"
 
