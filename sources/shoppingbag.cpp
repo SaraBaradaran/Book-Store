b213cf8d8bc9cd10e7681c47807807df8ac87d3d
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2021-05-25 20:36:54 +0430

    Update shoppingbag.cpp

--- sources/shoppingbag.cpp
+++ sources/shoppingbag.cpp
@@ -1,3 +1,14 @@
+/****************************************************************  
+#								
+# 								
+# 	Creator Name:   Sara Baradaran, Mahdi Heidari		
+# 	Create Date:    Nov 2018 				
+# 	Module Name:    shoppingbag.cpp
+# 	Project Name:   Bookstore	
+#								
+#								
+****************************************************************/
+
 #include "shoppingbag.h"
 
 ShoppingBag::ShoppingBag()
