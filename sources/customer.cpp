b8a57bb1ee62fd4650481ae36ee19ad2520df09e
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2021-05-25 20:34:35 +0430

    Update customer.cpp

--- sources/customer.cpp
+++ sources/customer.cpp
@@ -1,3 +1,14 @@
+/****************************************************************  
+#								
+# 								
+# 	Creator Name:   Sara Baradaran, Mahdi Heidari		
+# 	Create Date:    Nov 2018 				
+# 	Module Name:    customer.cpp
+# 	Project Name:   Bookstore	
+#								
+#								
+****************************************************************/
+
 #include "customer.h"
 
 Customer::Customer(int no,QString name):no(no),name(name)
